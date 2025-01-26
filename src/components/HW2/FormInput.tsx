ffc30f21a936e043c76cf134f90f53f8fbb53632
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-26 21:46:32 +0900

    [구현] FormInput 컴포넌트
    - Form 내부 input 자동완성, 자동채우기 기능 off
    - password입력 관련 input 상태관리로 비밀번호 표시 및 가리기 기능 추가
    - input별 placeholder 표시 및 값 변동 사항 추적
    - SignUp 컴포넌트에 연결

--- src/components/HW2/FormInput.tsx
+++ src/components/HW2/FormInput.tsx
@@ -1,37 +1,52 @@
-import EyeClosed from './EyeClosed';
+import { useState } from 'react';
+import ToggleVisiible from './ToggleVisible';
 
 interface FormInput {
   title?: string;
   name?: string;
   type?: string;
+  placeholder?: string;
   value: string;
   onChange: (value: string) => void;
-  isVisible?: boolean;
+  isToggle?: boolean;
 }
 
 function FormInput({
   title,
+  placeholder,
   name,
   type = 'text',
   value,
   onChange,
-  isVisible,
+  isToggle,
 }: FormInput) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange(e.target.value);
   };
 
+  const togglePassword = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <>
       <label htmlFor={name}>{title}</label>
       <div>
         <input
-          type={type}
+          autoComplete="new-password"
+          type={type === 'password' && passwordVisible ? 'text' : type}
           id={name}
-          defaultValue={value}
+          placeholder={placeholder}
+          value={value}
           onChange={handleFormInput}
         />
-        <EyeClosed onClick={() => {}} isVisible={isVisible} />
+        {isToggle && (
+          <ToggleVisiible
+            onClick={togglePassword}
+            isVisible={passwordVisible}
+          />
+        )}
       </div>
     </>
   );
--- src/components/HW2/SignUp.tsx
+++ src/components/HW2/SignUp.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+import FormButton from './FormButton';
 import FormInput from './FormInput';
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    validPassword: '',
+  });
+
+  const handleChange = (key: string, value: string) => {
+    setFormData({ ...formData, [key]: value });
+  };
+
   return (
     <section>
       <h2>회원가입 정보 입력 페이지</h2>
@@ -8,10 +21,38 @@ function SignUp() {
         <FormInput
           title="이름"
           type="text"
-          name="이름"
-          value="안녕"
-          onChange={(value) => console.log(value)}
+          name="userName"
+          placeholder="2글자 이상 입력"
+          value={formData.name}
+          onChange={(value) => handleChange('name', value)}
         />
+        <FormInput
+          title="이메일"
+          type="email"
+          name="userEmail"
+          placeholder="sungwoo@gmail.com"
+          value={formData.email}
+          onChange={(value) => handleChange('email', value)}
+        />
+        <FormInput
+          title="패스워드"
+          type="password"
+          name="userPassword"
+          placeholder="숫자, 영문 조합 6자리 이상 입력"
+          value={formData.password}
+          onChange={(value) => handleChange('password', value)}
+          isToggle
+        />
+        <FormInput
+          title="패스워드 확인"
+          type="password"
+          name="userPasswordValid"
+          placeholder="입력한 패스워드 다시 입력"
+          value={formData.validPassword}
+          onChange={(value) => handleChange('validPassword', value)}
+          isToggle
+        />
+        <FormButton text="회원가입" />
       </form>
     </section>
   );
