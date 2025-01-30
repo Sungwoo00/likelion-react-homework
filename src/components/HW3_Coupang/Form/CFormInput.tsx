c8245e4adf04855ac77b00b1e95dc69371cb70d7
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-30 13:43:56 +0900

    [구현] FormInput 에러 메세지 조건부 렌더링 및 CSS 추가

--- src/components/HW3_Coupang/Form/CFormInput.tsx
+++ src/components/HW3_Coupang/Form/CFormInput.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Icon from '../UI/Icon';
 import '../../../styles/Coupang/Form/CFormInput.css';
 
-interface FormInput {
+interface CFormInput {
   title?: string;
   name?: string;
   type?: string;
@@ -12,7 +12,7 @@ interface FormInput {
   isToggle?: boolean;
 }
 
-function FormInput({
+function CFormInput({
   title,
   placeholder,
   name,
@@ -20,43 +20,74 @@ function FormInput({
   value,
   onChange,
   isToggle,
-}: FormInput) {
+}: CFormInput) {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [error, setError] = useState('');
 
-  const handleFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value);
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleCFormInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    onChange(newValue);
+
+    if (newValue === '') {
+      setError('');
+      return;
+    }
+
+    if (type === 'email') {
+      if (!validateEmail(newValue) && newValue !== '') {
+        setError('아이디(이메일)는 이메일 형식으로 입력해주세요.');
+      } else {
+        setError('');
+      }
+    } else if (type === 'password') {
+      if (newValue.length < 6) {
+        setError('숫자, 영문 조합 6자리 이상 입력해 주세요.');
+      } else {
+        setError('');
+      }
+    }
   };
 
   const togglePassword = () => {
     setPasswordVisible(!passwordVisible);
   };
   return (
-    <div className="FormInput">
-      <label className="FormInput__label" htmlFor={name}>
-        <Icon shape={type === 'email' ? 'letter' : 'lock'} />
-        <span>{title}</span>
-      </label>
-      <div className="FormInput__container">
-        <input
-          className="FormInput__input"
-          autoComplete="new-password"
-          type={type === 'password' && passwordVisible ? 'text' : type}
-          id={name}
-          placeholder={placeholder}
-          value={value}
-          onChange={handleFormInput}
-        />
-        {isToggle && (
-          <Icon
-            onClick={togglePassword}
-            isVisible={passwordVisible}
-            shape={passwordVisible ? 'show' : 'hide'}
-            label="비밀번호"
+    <div className="CFormInput">
+      <div
+        className={`CFormInput__Wrapper ${error ? 'CFormInput__Wrapper--error' : ''}`}
+      >
+        <label className="CFormInput__label" htmlFor={name}>
+          <Icon shape={type === 'email' ? 'letter' : 'lock'} />
+          <span>{title}</span>
+        </label>
+        <div className="CFormInput__container">
+          <input
+            className="CFormInput__input"
+            autoComplete="new-password"
+            type={type === 'password' && passwordVisible ? 'text' : type}
+            id={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={handleCFormInput}
           />
-        )}
+          {isToggle && (
+            <Icon
+              onClick={togglePassword}
+              isVisible={passwordVisible}
+              shape={passwordVisible ? 'show' : 'hide'}
+              label="비밀번호"
+            />
+          )}
+        </div>
       </div>
+      {error && <p className="CFormInput__error">{error}</p>}
     </div>
   );
 }
 
-export default FormInput;
+export default CFormInput;
