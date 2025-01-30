e9cdb9609adc23a717e62b6af86810e01a0c219a
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-30 13:42:51 +0900

    [CSS] FormCheckBox 컴포넌트 CSS 수정 및 체크박스 조건부 렌더링

--- src/components/HW3_Coupang/Form/FormCheckbox.tsx
+++ src/components/HW3_Coupang/Form/FormCheckbox.tsx
@@ -1,31 +1,41 @@
 import isChecked from '../../../assets/Form/checkboxTrue.svg';
-import isUnChecked from '../../../assets/Form/checkboxFalse.svg';
+// import isUnChecked from '../../../assets/Form/checkboxFalse.svg';
+import Tooltip from '../UI/Tooltip';
+import '../../../styles/Coupang/Form/FormCheckBox.css';
 
 interface FormCheckBox {
   checked: boolean;
   disabled?: boolean;
+  tooltipText?: string;
   onChange: (checked: boolean) => void;
 }
 
-function FormCheckBox({ checked, disabled, onChange }: FormCheckBox) {
+function FormCheckBox({
+  checked,
+  disabled,
+  tooltipText,
+  onChange,
+}: FormCheckBox) {
   return (
-    <label className="FormCheckBox">
-      <div className="FormCheckBox__wrapper">
-        <input
-          type="checkbox"
-          className="FormCheckBox__input"
-          checked={checked}
-          disabled={disabled}
-          onChange={(e) => onChange(e.target.checked)}
-        />
-        <img
-          className="FormCheckBox__icon"
-          src={checked ? isChecked : isUnChecked}
-          alt=""
-        />
-      </div>
-      <span>자동 로그인</span>
-    </label>
+    <div className="FormCheckBox">
+      <input
+        type="checkbox"
+        className="FormCheckBox__input"
+        id="autoLogin"
+        checked={checked}
+        disabled={disabled}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <label className="FormCheckBox__label" htmlFor="autoLogin">
+        {checked ? (
+          <img className="FormCheckBox__icon" src={isChecked} alt="" />
+        ) : (
+          <div className="FormCheckBox__box" />
+        )}
+        <span className="FormCheckBox__text">자동 로그인</span>
+      </label>
+      {checked && tooltipText && <Tooltip text={tooltipText} direction="top" />}
+    </div>
   );
 }
 
