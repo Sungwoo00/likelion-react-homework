22c6d92e63653fc65f4db3a354f93ab57f541bcf
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-30 13:48:20 +0900

    [구현] CoupangSIgnInForm 컴포넌트 CFormButton 조건부 CSS변경 로직 처리 및 css추가
    - 필요 SVG 추가

--- src/components/HW3_Coupang/CoupangSignIn.tsx
+++ src/components/HW3_Coupang/CoupangSignIn.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import FormCheckBox from '../HW3_Coupang/Form/FormCheckBox';
 import CFormButton from './Form/CFormButton';
 import CFormInput from './Form/CFormInput';
-import Divider from '../HW3_Coupang/UI/Divider';
-import Tooltip from './UI/Tooltip';
+import Divider from './UI/Divider';
+import symbol from '../../assets/UI/symbol.svg';
+import coupang from '../../assets/UI/coupang.svg';
+import '../../styles/Coupang/CoupangSignIn.css';
 
-function SignIn() {
+function CoupangSignIn() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -15,47 +17,73 @@ function SignIn() {
   const handleChange = (key: string, value: string | boolean) => {
     setFormData({ ...formData, [key]: value });
   };
+  console.log('formData:', formData);
 
   const handleSubmit = () => {
     console.log(formData);
   };
 
+  const isFormValid = formData.email && formData.password;
   return (
     <section>
-      <h2 className="sr-only">로그인 정보 입력 페이지</h2>
-      <form className="SignIn" action={handleSubmit}>
-        <div className="SignInForm">
+      <h2>
+        <span className="sr-only">쿠팡 로그인 사이트</span>
+        <img
+          width="256"
+          height="56"
+          src={coupang}
+          alt=""
+          className="CoupangSignIn__logo"
+        />
+      </h2>
+      <form className="CoupangCoupangSignIn" action={handleSubmit}>
+        <div className="CoupangCoupangSignInForm">
           <CFormInput
             title="아이디"
             type="email"
-            name="SignIn_email"
+            name="CoupangSignIn_email"
             placeholder="아이디(이메일)"
             value={formData.email}
             onChange={(value) => handleChange('email', value)}
           />
-          <Divider />
           <CFormInput
             title="비밀번호"
             type="password"
-            name="SignIn_password"
+            name="CoupangSignIn_password"
             placeholder="비밀번호"
             value={formData.password}
             onChange={(value) => handleChange('password', value)}
             isToggle
           />
-          <Divider />
-          <div className="SignInForm__checkbox">
+          <div className="CoupangSignInForm__checkbox">
             <FormCheckBox
               checked={formData.autoLogin}
+              tooltipText="개인 정보 보호를 위해 본인 기기에서만 이용해주세요"
               onChange={(checked) => handleChange('autoLogin', checked)}
             />
-            <Tooltip text="개인 정보 보호를 위해 본인 기기에서만 이용해주세요" />
+            <div className="CoupangSignInForm__find">
+              <a href="#" className="CoupangSignInForm__find-link">
+                아이디(이메일)/비밀번호 찾기
+                <img
+                  src={symbol}
+                  alt=""
+                  className="CoupangSignInForm__find-arrow"
+                  aria-hidden="true"
+                />
+              </a>
+            </div>
           </div>
         </div>
-        <CFormButton text="로그인" />
+        <CFormButton
+          text="로그인"
+          mode={isFormValid ? 'Primary' : undefined}
+          disabled={!isFormValid}
+        />
+        <Divider />
+        <CFormButton text="회원가입" mode="Secondary" disabled={false} />
       </form>
     </section>
   );
 }
 
-export default SignIn;
+export default CoupangSignIn;
