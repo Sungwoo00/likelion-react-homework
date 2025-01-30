2cac05759231178ea4faac7667dc442755ac0edb
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-30 13:41:21 +0900

    [CSS] Tooltip 컴포넌트 CSS 추가

--- src/components/HW3_Coupang/UI/Tooltip.tsx
+++ src/components/HW3_Coupang/UI/Tooltip.tsx
@@ -2,13 +2,14 @@ import TooltipRight from '../../../assets/UI/TooltipRight.svg';
 import TooltipLeft from '../../../assets/UI/TooltipLeft.svg';
 import TooltipBottom from '../../../assets/UI/TooltipBottom.svg';
 import TooltipTop from '../../../assets/UI/TooltipTop.svg';
+import '../../../styles/Coupang/UI/Tooltip.css';
 
 interface Tooltip {
   text: string;
   direction?: 'top' | 'bottom' | 'right' | 'left';
 }
 
-function Tooltip({ text, direction = 'left' }: Tooltip) {
+function Tooltip({ text, direction = 'top' }: Tooltip) {
   const getTooltip =
     direction === 'right'
       ? TooltipRight
@@ -19,11 +20,16 @@ function Tooltip({ text, direction = 'left' }: Tooltip) {
           : TooltipTop;
 
   return (
-    <div className={`Tooltip Tooltip--${direction}`}>
+    <div className="Tooltip">
       <span className="Tooltip__text">{text}</span>
-      <img className="Tooltip__arrow" src={getTooltip} alt="" />
+      <img
+        className={`Tooltip__arrow Tooltip--${direction}`}
+        src={getTooltip}
+        alt=""
+      />
     </div>
   );
 }
 
 export default Tooltip;
+2;
