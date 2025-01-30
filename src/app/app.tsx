b85320b0efb4a60af66a3fc87c4f757052f5f7df
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-01-30 13:50:14 +0900

    [CSS] 렌더링 페이지 css 변경

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,12 @@
 import SignUp from '@/components/HW3_FormUI/SignUp';
 import SignIn from '@/components/HW3_FormUI/SignIn';
+import CoupangCoupangSignIn from '@/components/HW3_Coupang/CoupangSignIn';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <CoupangCoupangSignIn />
       <SignUp />
       <SignIn />
     </div>
