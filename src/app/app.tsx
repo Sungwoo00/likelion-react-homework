cb3efe025e092ed2a683ff673ebabdd765733bfc
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-02-07 00:40:40 +0900

    [수정] Cart 컴포넌트로 묶기

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,13 +2,15 @@ import SignUp from '@/components/HW3_FormUI/SignUp';
 import SignIn from '@/components/HW3_FormUI/SignIn';
 import CoupangCoupangSignIn from '@/components/HW3_Coupang/CoupangSignIn';
 import './app.css';
+import Cart from '@/components/HW4.Cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <CoupangCoupangSignIn />
+      {/* <CoupangCoupangSignIn />
       <SignUp />
-      <SignIn />
+      <SignIn /> */}
+      <Cart/>
     </div>
   );
 }
