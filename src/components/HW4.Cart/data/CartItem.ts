4810b64ddb854a05a5279331c4db7aecefac6c27
sungwoo00 <dhdpdhdp5899@gmail.com>
2025-02-09 16:11:14 +0900

    이미지 추가 및 경로 설정 변경

--- src/components/HW4.Cart/data/CartItem.ts
+++ src/components/HW4.Cart/data/CartItem.ts
@@ -10,21 +10,21 @@ export interface CartItem{
 export const CartItems: CartItem[] = [
   {
     id: 1,
-    src: "src/components/HW4.Cart/components/UI/image2.jpg",
+    src: "src/components/HW4.Cart/components/UI/image-2.jpg",
     title: "1A 우유 900mL",
     price: 1800,
     quantity: 2,
   },
   {
     id: 2,
-    src: "src/components/HW4.Cart/components/UI/image3.jpg",
+    src: "src/components/HW4.Cart/components/UI/image-3.jpg",
     title: "맛있는 콩나물 500g",
     price: 600,
     quantity: 3,
   },
   {
     id: 3,
-    src: "src/components/HW4.Cart/components/UI/image4.jpg",
+    src: "src/components/HW4.Cart/components/UI/image-4.jpg",
     title: "고소한 두부 1kg",
     price: 800,
     quantity: 5,
